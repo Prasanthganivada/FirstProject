7979f56ffc6af17ba526d35732d016c7644c8d85
Prasanth <Prasanth@DESKTOP-LA9THFC.Dlink>
2022-03-13 16:32:56 +0530

    First Commit

---
+++ src/demoProgram/FirstProgram.java
@@ -0,0 +1,27 @@
+package demoProgram;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		int i=10;
+		float myFloatNumber = 5.66f;
+		char myChar='a';
+		String myText="Hello";
+		
+		System.out.println(i);
+		System.out.println(myFloatNumber);
+		System.out.println(myChar);
+		System.out.println(myText);
+
+	}
+	public void SecondProgram() {
+	//	WebDriver driver = new WebDriver();
+	//	driver.manage().windows().maximize();
+	final int i=20;
+
+	System.out.println(i);
+
+
+	}
+
+}
